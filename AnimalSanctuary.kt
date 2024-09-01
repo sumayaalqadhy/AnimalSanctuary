bb0876b44c4169d57a7def04e0dd505f20a0d21b
Sumaya Alqadhy <sumayaalqadhy@Sumayas-MacBook-Pro.local>
2024-09-01 12:15:42 -0700

    added time tracking variables

--- AnimalSanctuary.kt
+++ AnimalSanctuary.kt
@@ -2,4 +2,10 @@ fun main() {
   // Write your code below 🏞
   var responsibilities = listOf("feed the chimps", "play a random game", "conduct a health check on Foxie")
 
+  var responsibilitiesComplete = 0
+
+  var timeSpent = 0
+
+  val totalShiftTime = 4
+
 }
